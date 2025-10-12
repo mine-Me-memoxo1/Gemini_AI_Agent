93d83b89d962f0b4bf79ec33e347a5b12d558bdf
labhi10.20 <labhi10.20@proton.me>
2025-10-12 10:36:25 -0400

    Removed hardcoded prompt by taking it as arguments when running file. Updated prompt to keep track of history (like a conversation), and added verbose flag

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+user_prompt = None
+if len(sys.argv) == 1:
+    raise Exception('No prompt given')
+else:
+    user_prompt = sys.argv[1]
+   
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents = messages)
 
 print(response.text)
-print(type(response.usage_metadata))
-print('Prompt tokens: {}'.format(response.usage_metadata.prompt_token_count))
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+if '--verbose' in sys.argv[2:]:
+    print('User prompt: {}'.format(user_prompt))
+    print('Prompt tokens: {}'.format(response.usage_metadata.prompt_token_count))
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
